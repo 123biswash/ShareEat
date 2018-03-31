327e401c60afff600fa7d75bcd7ee59ceda7742b
Biswash Adhikari <biswash.adhikari@bison.howard.edu>
2018-03-31 09:34:36 -0400

    with nothing

---
+++ ShareEat/View Controllers/AnimateViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AnimateViewController.swift
+//  ShareEat
+//
+//  Created by Biswash Adhikari on 3/31/18.
+//  Copyright © 2018 Biswash Adhikari. All rights reserved.
+//
+
+import UIKit
+
+class AnimateViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //perform(Selector("showNext"), with: nil, afterDelay: 3)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+//    func showNext(){
+//        performSegue(withIdentifier: "segueToNextView" , sender: self)
+//    }
+   
+    
+   
+   
+    
+   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
